bbb5c7e2b6819ebd4dcb91a025b812b61ab7bc15
Krystin Villeneuve <krystinv@gmail.com>
2017-02-09 10:55:35 -0700

    Cleanup on linter

--- tests/deck-test.js
+++ tests/deck-test.js
@@ -4,39 +4,32 @@ import Card from '../scripts/card'
 
 
 describe('Deck', () => {
-  let card1 = new Card("What is the capital of Alaska?", "Juneau")
-  let card2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
-  let card3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+  let card1 = new Card('What is the capital of Alaska?', 'Juneau')
+  let card2 = new Card('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars')
+  let card3 = new Card('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west')
   let deck = new Deck([card1, card2, card3])
 
-    it('should be an instance of Deck', () => {
-      let deck = new Deck()
-      expect(deck).to.be.an.instanceOf(Deck)
-    })
+  it('should be an instance of Deck', () => {
+    let deck = new Deck()
+    expect(deck).to.be.an.instanceOf(Deck)
+  })
 
-    // it('should take in an array', () => {
-    //   let deck = new Deck ([])
-    //   assert.isArray(deck)
-    // })
+  it('should hold cards 1, 2, & 3', () => {
+    expect(deck.cards).to.deep.equal([card1, card2, card3])
+  })
 
-    it('should hold cards 1, 2, & 3', () => {
-      expect(deck.cards).to.deep.equal([card1, card2, card3])
-    })
+  it('should have a count function', () => {
+    assert.isFunction(deck.count)
+  })
 
-    it('should have a count function', () => {
-      assert.isFunction(deck.count)
-    })
-
-    it('should have a correct count', () => {
-      expect(deck.count()).to.equal(3)
-    })
+  it('should have a correct count', () => {
+    expect(deck.count()).to.equal(3)
+  })
 })
 
 describe('From File', () => {
-  const filename = "cards.txt"
+  const filename = 'cards.txt'
   let deckFromFile = fromFile(filename)
-  // let cardsGenerator = new CardGenerator(filename)
-  // let cards = cardsGenerator.cards
 
   it('should have four cards', () => {
     expect(deckFromFile.cards.length).to.equal(4)
--- tests/guess-test.js
+++ tests/guess-test.js
@@ -1,4 +1,4 @@
-import { expect, assert } from 'chai'
+import { expect } from 'chai'
 import Card from '../scripts/card'
 import Guess from '../scripts/guess'
 
@@ -11,36 +11,35 @@ describe('Guess', () => {
     expect(guess).to.be.an.instanceOf(Guess)
   })
 
-    it('should have property: response', () => {
-      expect(guess).to.have.property('response')
-    })
+  it('should have property: response', () => {
+    expect(guess).to.have.property('response')
+  })
 
-    it('should be able to provide a response from the game player',   () => {
-      expect(guess.response).to.equal('Juneau')
-    })
+  it('should be able to provide a response from the game player',   () => {
+    expect(guess.response).to.equal('Juneau')
+  })
 
-    it('should be able to provide correct', ()=>{
-      expect(guess.correct).to.be.ok
-    })
+  it('should be able to provide correct', ()=>{
+    expect(guess.correct).to.be.ok
+  })
 
-    it('should provide feedback()', ()=>{
-      expect(guess.feedback()).to.deep.equal('Correct!')
-    })
+  it('should provide feedback()', ()=>{
+    expect(guess.feedback()).to.deep.equal('Correct!')
+  })
 
-    // End of Iteration II spec A
-describe('Guess', () => {
+  describe('Guess', () => {
     // Begin Iteration II spec B
 
-    let card = new Card("Which planet is closest to the sun?", "Mercury")
-    let guess = new Guess("Saturn", card)
+    let card = new Card('Which planet is closest to the sun?', 'Mercury')
+    let guess = new Guess('Saturn', card)
 
     it('should receive Saturn as a user response', () => {
       expect(guess.response).to.equal('Saturn')
     })
 
     it('should return correct when the response matches the answer', () => {
-      expect(!guess.correct);
-  })
+      expect(!guess.correct)
+    })
     it('should provide a valid feedback() based on user response', ()=> {
       expect(guess.feedback()).to.deep.equal('Incorrect.')
     })
